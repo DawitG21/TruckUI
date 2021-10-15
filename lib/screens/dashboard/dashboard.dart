1bc5e0b9c26c78f73ed065d313e037e40974d631
muluken <mulukent@appliedline.com>
2021-10-15 16:42:27 +0300

    Name Change

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -13,7 +13,7 @@ class _DashBoardState extends State<DashBoard> {
     // ignore: avoid_unnecessary_containers
     return Container(
       child: const Text(
-        'Dummy Dashboard data',
+        'Dummy Dashboard Data',
       ),
     );
   }
