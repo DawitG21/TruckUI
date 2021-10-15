e90145563e9e6a7bdffc272ac59c8f645b9de8a2
usmanumer32 <usmanumer32@gmail.com>
2021-10-15 09:12:37 +0300

    dashboard

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -13,7 +13,7 @@ class _DashBoardState extends State<DashBoard> {
     // ignore: avoid_unnecessary_containers
     return Container(
       child: const Text(
-        'Dummy data',
+        'Dummy Dashboard data',
       ),
     );
   }
